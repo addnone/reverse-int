98770d2bcf4953103fa2cd2e0d2946f233f2e047
vsavin <savinvitya@gmail.com>
2021-03-15 16:53:15 +0300

    feat: add main logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    // sign = n > 0 ? "-" : ""
+    string = n > 0 ? n.toString() : (-n).toString()
+    return Number(string.split('').reverse().join(''))
 }
